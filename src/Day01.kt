656c679139519ab4548a4c8457114e904399a83d
Alessandro D'Amico <alessandro.damico@intre.it>
2022-12-01 18:00:46 +0100

    day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,35 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun headAndTail(input: List<String>): Pair<List<String>, List<String>> {
+        val head = input.takeWhile { it.isNotBlank() }
+        val tail = input.dropWhile { it.isNotBlank() }.drop(1)
+        return head to tail
     }
+
+    fun splitOnSpaces(input: List<String>): List<List<String>> {
+        val (head, tail) = headAndTail(input)
+        if (tail.isEmpty())
+            return listOf(head)
+        return splitOnSpaces(tail).plus(listOf(head))
+    }
+
+    fun toCalories(input: List<String>): List<Int> {
+        val groups = splitOnSpaces(input)
+        val calories = groups.map { group: List<String> ->
+            group.sumOf { it.toInt() }
+        }
+        return calories
+    }
+
+    fun part1(input: List<String>) =
+        toCalories(input).maxOf { it }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val calories = toCalories(input).sortedDescending()
+        return calories.take(3).sum()
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
 }
+
